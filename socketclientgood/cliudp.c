7ad900c64b9a61b0c9df56f5382f2179a8b2ecc9
Mooneba <contal.cecile@gmail.com>
2015-05-03 20:15:38 +0200

    update

---
+++ socketclientgood/cliudp.c
@@ -0,0 +1,129 @@
+/*****
+* Exemple de client UDP
+* socket en mode non connecte
+*****/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/mman.h>
+#include <native/task.h>
+#include <native/timer.h>
+
+#define C1SEC 50000000 /* 1 Sec en nano-sec */
+struct sockaddr_in Sock;
+int sid;
+
+static void f_task(void *arg)
+{
+volatile unsigned int i=0;
+unsigned int n=0;
+unsigned long int out=0;
+unsigned long long int start = 0;
+RT_TIMER_INFO timer_info;
+
+  if (rt_task_set_periodic(
+      NULL,   /* la tache courrante */
+      TM_NOW, /* delai de mise en place, TM_NOW = none */
+      C1SEC    /* valeur en nanosecondes */) != 0)
+  {
+    printf("rt_task_set_periodic error\n");
+    return;
+  }
+
+  rt_timer_inquire(&timer_info);
+  start = timer_info.date/C1SEC;
+  unsigned long long int time = 0;
+
+  while (n<130) { //130 périodes
+    /* secondary mode */
+    rt_timer_inquire(&timer_info);
+
+    /* Affiche un message toutes les secondes */
+    time = timer_info.date/C1SEC;
+    char str[15];
+    sprintf(str, "%d", time);
+    if (sendto(sid,(str),strlen(str),0,(struct sockaddr *)&Sock,
+                           sizeof(Sock))==-1) {
+        perror("sendto");
+        return(4);
+    }
+    printf("Envoi OK !\n");
+    n++;
+
+    out = 0;
+    /* Retour en primary mode a cause d'un appel systeme Xenomai */
+    switch (rt_task_wait_period(&out)) {
+      case 0:
+      case -ETIMEDOUT:
+        if (out) printf("Overrun: %lu cycles perdus\n",out); /* 2nd. mode */
+        break;
+      default:
+        printf("ERREUR rt_task_wait_period !\n"); /* 2nd. mode */
+        return;
+    }
+  }
+}
+
+/* parametres :
+        P[1] = nom de la machine serveur
+        P[2] = port
+        P[3] = message
+*/
+int main(int N, char*P[])
+{
+    struct hostent *h;
+    if (N != 3) {
+        fprintf(stderr,"Utilisation : %s nom_serveur port\n", P[0]);
+        return(1);
+    }
+    /* creation du socket */
+    if ((sid=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)) < 0) {
+        perror("socket");
+        return(2);
+    }
+    /* recuperation adresse du serveur */
+    if (!(h=gethostbyname(P[1]))) {
+        perror(P[1]);
+        return(3);
+    }
+
+    bzero(&Sock,sizeof(Sock));
+    Sock.sin_family = AF_INET;
+    bcopy(h->h_addr,&Sock.sin_addr,h->h_length);
+    Sock.sin_port = htons(atoi(P[2]));
+
+    if (sendto(sid,("Good"),strlen("Good"),0,(struct sockaddr *)&Sock,
+                           sizeof(Sock))==-1) {
+        perror("sendto");
+        return(4);
+    }
+    printf("Envoi OK !\n");
+
+
+    RT_TASK t_id;
+    int n;
+
+    mlockall(MCL_CURRENT | MCL_FUTURE); /* pour ne pas swapper */
+
+    if ((n=rt_task_spawn(&t_id,"maTache",0,99,T_JOINABLE,&f_task,NULL))!=0) {
+    printf("ERREUR rt_task_spawn %d!\n",n);
+    return 1;
+    }
+    rt_task_join(&t_id);
+
+    if (sendto(sid,("FIN"),strlen("FIN"),0,(struct sockaddr *)&Sock,
+                           sizeof(Sock))==-1) {
+        perror("sendto");
+        return(4);
+    }
+    printf("Envoi OK !\n");
+
+    close(sid);
+    return 0;
+}
+
---
+++ socketserveurgood/servudp.c
@@ -0,0 +1,67 @@
+/*************************************************
+* exemple de serveur UDP
+* socket non connecte
+*************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "sint.h"
+
+
+#define LBUF 1024
+#define PORT 9999
+
+struct sockaddr_in Sock = {AF_INET}; /* le reste est nul */
+
+char * add_ip(unsigned int A)
+{
+static char b[16];
+   sprintf(b,"%u.%u.%u.%u",(unsigned int)(A>>24),(unsigned int)((A>>16)&0xFF),
+             (unsigned int)((A>>8)&0xFF),(unsigned int)(A&0xFF));
+   return b;
+}
+
+
+int main(int N, char*P[])
+{
+int sid, lg, n;
+char buf[LBUF];
+struct sockaddr_in Srec;
+
+    /* creation du socket */
+    if ((sid = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    /* si on veut forcer le no du port alors decommenter ce qui suit
+    Sock.sin_port = htons(PORT);
+    */
+    /* attachement du socket */
+    if (bind(sid, (struct sockaddr*)&Sock, sizeof(Sock)) < 0) {
+       perror("bind");
+       exit(2);
+    }
+    /* on recupere le no du port */
+    lg = sizeof(Sock);
+    if(getsockname(sid,(struct sockaddr *)&Sock,(socklen_t*)&lg) < 0) {
+        perror("getsockname");
+        exit(3);
+    }
+    printf("Le serveur est attache au port %u\n", ntohs(Sock.sin_port));
+     creationFichier();
+    /* boucle de lecture des messages des clients */
+    do  {
+        lg = sizeof(Srec);
+        n = recvfrom(sid,buf,LBUF,0,(struct sockaddr *)&Srec,(socklen_t*)&lg);
+        if (n < 0) perror("recvfrom");
+        else {
+          buf[n] = '\0';
+          construction(atof(buf));
+          printf("Recu de %s : <%s>\n",add_ip(ntohl(Srec.sin_addr.s_addr)),buf);
+    }
+    } while (buf[0]!='F');
+     fermerFichier();
+    close(sid);
+}
